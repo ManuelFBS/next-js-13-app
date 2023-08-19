7bf1c53860313ad0cd706a9c5c7383e28b405943
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-19 04:24:26 -0400

    Actualizacion de page.jsx (posts)

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,7 +1,20 @@
-/**
- * La función exporta un componente React que representa un elemento de encabezado con el texto "Estos
- * son los Posts...".
- */
-export default function PostsPage() {
-  return <h1>Aquí se muestran los Posts...</h1>;
+const fetchPosts = () => {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
+  );
+};
+
+export default async function PostsPage({ params }) {
+  const posts = await fetchPosts();
+
+  return (
+    <section>
+      {posts.slice(0, 5).map((post) => (
+        <article key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </article>
+      ))}
+    </section>
+  );
 }
