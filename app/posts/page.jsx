8ab29ab35ebb1a8876a3705138cb10f98ba10ee3
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-21 01:38:30 -0400

    Creac. ListOfPosts.jsx para llamar desde page.jsx

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,20 +1,9 @@
-const fetchPosts = () => {
-  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
-    res.json()
-  );
-};
+import ListOfPosts from "./ListOfPosts";
 
 export default async function PostsPage({ params }) {
-  const posts = await fetchPosts();
-
   return (
     <section>
-      {posts.slice(0, 5).map((post) => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </article>
-      ))}
+      <ListOfPosts />
     </section>
   );
 }
