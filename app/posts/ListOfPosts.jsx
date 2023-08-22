9afa3697abbe86504b6930cdbc1f785a53ed5f65
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-22 17:53:55 -0400

    Actualizacion de ListOfPosts.jsx

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,9 +1,17 @@
+import Link from 'next/link'
 import LikeButton from './LikeButton'
 
 const fetchPosts = () => {
-  return fetch('https://jsonplaceholder.typicode.com/posts').then((res) =>
-    res.json()
-  )
+  // console.log('FETCHING POSTS !!!!!!')
+  // console.log('🙀')
+  // return fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+  //   .then((res) => res.json())
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60
+    }
+  })
+    .then((res) => res.json())
 }
 
 export default async function ListOfPosts ({ params }) {
@@ -11,9 +19,11 @@ export default async function ListOfPosts ({ params }) {
 
   return posts.slice(0, 5).map((post) => (
     <article key={post.id}>
-      <h2 style={{ color: '#09f' }}>{post.title}</h2>
-      <p>{post.body}</p>
-      <LikeButton id={post.id} />
+      <Link href='/posts/[id]' as={`/posts/${post.id}`}>
+        <h2 style={{ color: '#09f' }}>{post.title}</h2>
+        <p>{post.body}</p>
+        <LikeButton id={post.id} />
+      </Link>
     </article>
   ))
 }
