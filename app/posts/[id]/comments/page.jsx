5bf6238259f2cdf4a8b50ecf634023446735b792
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-25 13:55:01 -0400

    Actualizacion de page.jsx (comments)

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,4 +1,4 @@
-const fetchSinglePost = (id) => {
+const fetchComments = (id) => {
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
             revalidate: 60
@@ -9,14 +9,14 @@ const fetchSinglePost = (id) => {
 
 export default async function Post ({ params }) {
   const { id } = params
-  const comments = await fetchSinglePost(id)
+  const comments = await fetchComments(id)
 
   return (
-    <ul>
+    <ul style={{ background: '#444', fontSize: '12px' }}>
       {comments.map(comment => (
         <li key={comment.id}>
-          <h2>{comment.name}</h2>
-          <p>{comment.body}</p>
+          <h4>{comment.name}</h4>
+          <small>{comment.body}</small>
         </li>
       ))}
     </ul>
