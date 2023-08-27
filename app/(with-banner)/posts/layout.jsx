17c222aa0973fb107b0e4020525d7c818b9667b3
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-27 11:30:45 -0400

    Modif. de rutas About y Posts

---
+++ app/(with-banner)/posts/layout.jsx
@@ -0,0 +1,21 @@
+// import { Counter } from './Counter'
+
+const PostsLayout = ({ children }) => {
+  return (
+    <div style={{ marginTop: '5px' }}>
+      {/* <Counter /> */}
+      {/* <small>Home &bull; Posts</small> */}
+      <marquee style={{ 
+        background: '#00b', 
+        fontFamily: 'Singapore Sling Expanded', 
+        fontStyle: 'italic', 
+        fontSize: '18px', 
+        color: '#ef0' }}>
+          Curso de Next.js @midudev: Mi primera app...
+      </marquee>
+      {children}
+    </div>
+  )
+}
+
+export default PostsLayout
