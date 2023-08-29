9e824f4fd44b5966fc07aaa98c1cdf90e0803666
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-29 12:37:59 -0400

    Creacion de font.js y modifc layout.js (app)

---
+++ app/font.js
@@ -0,0 +1,7 @@
+// import { Roboto } from 'next/font/google'
+import { Space_Grotesk as spaceGrotesk } from 'next/font/google'
+
+export const font = spaceGrotesk({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,9 @@
 import '../styles/globals.css'
 // import { Inter } from 'next/font/google'
-// import { Space_Grotesk } from 'next/font/google'
-import { Roboto } from 'next/font/google'
+import { font } from './font.js'
 import Navigation from '../components/Navigation'
 
 // const inter = Inter({ subsets: ['latin'] })
-// const font = Space_Grotesk({
-//   weight: ['400', '700'],
-//   subsets: ['latin']
-// })
-const font = Roboto({
-  weight: ['400', '700'],
-  subsets: ['latin']
-})
 
 export const metadata = {
   title: 'My first App with Next 13',
